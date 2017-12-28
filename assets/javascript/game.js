00ee17136c543106941e0abdf736478d29fc18a5
Conrad Harrison <a.conradharrison@gmail.com>
2017-12-28 15:42:10 -0600

    Add files via upload
    cleaned up scripting, removed extra variable definitions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,4 @@
-
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
 
 var wins = 0;
 var losses = 0;
@@ -9,27 +7,20 @@ var guessesLeft = 9;
 var guessedLetters = [];
 var letterToGuess = null;
 
-
-
-
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-
 var updateGuessesLeft = function() {
 
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
 var updateLetterToGuess = function() {
-  this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+  letterToGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 };
 var updateGuessesSoFar = function() {
- 
+
   document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
 
 var reset = function() {
-  totalGuesses = 9;
   guessesLeft = 9;
   guessedLetters = [];
 
@@ -41,29 +32,26 @@ var reset = function() {
 updateLetterToGuess();
 updateGuessesLeft();
 
-
-
 document.onkeyup = function(event) {
-    guessesLeft--;
+  guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
   guessedLetters.push(userGuess);
   updateGuessesLeft();
   updateGuessesSoFar();
 
-        if (guessesLeft > 0){
-            if (userGuess == letterToGuess){
-                wins++;
-                document.querySelector('#wins').innerHTML = "Wins: " + wins;
-                alert("Good guess.  You might be Psyhic!  Keep playing to know for sure.");
-                reset();
-            }
-        }else if(guessesLeft == 0){
-           
-            losses++;
-            document.querySelector('#losses').innerHTML = "Losses: " + losses;
-            alert("Well, no luck this time.  It doesn't mean you aren't psychic, keep trying!");
-          
+  if (userGuess == letterToGuess){
+            wins++;
+            document.querySelector('#wins').innerHTML = "Wins: " + wins;
+            alert("Good guess.  You might be Psyhic!  Keep playing to know for sure.");
             reset();
         }
+    if(guessesLeft == 0){
+
+        losses++;
+        document.querySelector('#losses').innerHTML = "Losses: " + losses;
+        alert("Well, no luck this time.  It doesn't mean you aren't psychic, keep trying!");
+
+        reset();
+    }
 };
